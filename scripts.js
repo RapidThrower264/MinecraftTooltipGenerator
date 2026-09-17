4d65305c2d39bb497b92c45e27cff9c85ec7274a
RapidThrower264 <42883326+RapidThrower264@users.noreply.github.com>
2026-09-18 08:34:14 +1200

    t-fix: adds regex to remove the ID section of an NBT string
    Signed-off-by: RapidThrower264 <42883326+RapidThrower264@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -2193,9 +2193,10 @@ window.addEventListener("load", async (event) => {
         try {
             // order of operations for regex a) fix lists containing B, I or S. b) remove any string wrapped json strings c) wrap keys with quotation marks. d) remove boolean, short, int, long, float or double for data types
             //data = data.replace(/(^\s*[a-zA-Z:_0-9]*:\s)\[[BIS];/gm, "$1[").replace(/(^\s*[a-zA-Z_0-9]*: )'[^']*'(|,)/gm, "$1\"deleted\"$2").replace(/^\s*([a-zA-Z:_0-9]*):\s/gm, '"$1": ').replace(/:\s*(-?\d+(?:\.\d+)?[bBsSlLfFdD]?)/g, ': "$1"');
-            data = data.replace(/([{,])([a-zA-Z]+)/gm, "$1\"$2\"").replace(/:([0-9]+)([bL])/gm, ":\"$1$2\"");
+            data = data.replace(/id:\[[0-9,I;-]+\],/gm, "").replace(/([{,])([a-zA-Z]+)/gm, "$1\"$2\"").replace(/:([0-9]+)([bL])/gm, ":\"$1$2\"");
             json = JSON.parse(data);
         } catch (error) {
+	    console.log(error);
             const errorData = createDebugInformation("jsonParsing", `Could not parse the content ${data}.`);
             errorData.error = error;
             createToast("error", "The item data that you copied doesn't seem to be valid.", "You can follow the guide <a href='https://www.youtube.com/watch?v=PEChxeNL2R0' target='_blank'>here</a> to find out how to do this!", "Click me to copy relevant debug data to your clipboard", errorData);
